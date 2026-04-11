5ef86c68bc8a3c4b66559d58ed976b20a421d09e
Guilain Leduc <guilain.leduc@uni.lu>
2026-04-11 19:02:23 +0200

    Update HelloController to return a personalized greeting based on APP_USER environment variable

--- src/main/java/org/example/iutprojectdocker/HelloController.java
+++ src/main/java/org/example/iutprojectdocker/HelloController.java
@@ -7,10 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 
+    /**
+     * @return A greeting message that includes the value of the "APP_USER" environment variable, or "World" if it is not set.
+     */
     @GetMapping("/")
     public String hello() {
         try {
-            return "Hello World";
+            String appUser = System.getenv("APP_USER");
+            String name = (appUser == null || appUser.isEmpty()) ? "World" : appUser;
+
+            String base = String.format("Hello %s!", name);
+            return base;
+
         } catch (Exception e) {
             return "Error: " + e.getMessage();
         }
